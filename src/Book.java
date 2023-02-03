037390880f411651e6686f7aa24bead9051a1621
Vally <murkhonovbatoha@gmail.com>
2023-02-04 01:42:26 +0800

    Body

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private final Author author;
+    private final String title;
+    private int publishingBook;
+
+    public Book(Author author, String title, int publishingBook) {
+        this.author = author;
+        this.title = title;
+        this.publishingBook = publishingBook;
+    }
+
+    //getter
+    public Author getAuthor() {
+        return author;
+    }
+    //getter
+    public String getTitle() {
+        return title;
+    }
+    //getter
+    public int getPublishingBook() {
+        return publishingBook;
+    }
+    //setter-(только на дату т.к. нет private)
+    public void setPublishingBook(int publishingBook) {
+        this.publishingBook = publishingBook;
+    }
+}
